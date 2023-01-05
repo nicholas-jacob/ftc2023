757729cc4e2bbfb2ac88411caa87b54a4a196e13
nicholas-jacob <nicklefish7@gmail.com>
2023-01-05 15:20:47 -0700

    started Teleop2023V3

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2023V3.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class Teleop2023V3 extends OpMode{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
