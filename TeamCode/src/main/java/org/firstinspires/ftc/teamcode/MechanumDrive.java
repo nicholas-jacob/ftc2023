6f7e913abf819d0e8fd36d939560d58ce595e48c
nicholas-jacob <nicklefish7@gmail.com>
2023-01-07 22:20:38 -0700

    MechanumDrive.java is finished but untested No longer uses Hardware.java as new method of defining variables is being tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
@@ -1,8 +1,39 @@
 package org.firstinspires.ftc.teamcode;
-public class MechanumDrive {
-    double xSource;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-    public MechanumDrive() {
-        xSource = 0.1;
+
+public class MechanumDrive {
+    public DcMotorEx frontRightMotor=null;
+    public DcMotorEx frontLeftMotor = null;
+    public DcMotorEx backRightMotor = null;
+    public DcMotorEx backLeftMotor = null;
+    public MechanumDrive(DcMotorEx frontLeftMotor, DcMotorEx frontRightMotor, DcMotorEx backRightMotor, DcMotorEx backLeftMotor) {
+        frontRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        frontLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        frontRightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        frontRightMotor.setPower(0);
+        frontLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+    }
+    //Takes in x y and rotation each with range [-1,1] and sets the 4 corners of the chasis to the correct power
+    public void Drive (double X, double Y, double W) {
+        double denominator=Math.max(Math.abs(Y) + Math.abs(X) + Math.abs(W), 1);
+        frontRightMotor.setPower((Y-X-W)/denominator);
+        frontLeftMotor.setPower((Y+X+W)/denominator);
+        backRightMotor.setPower((Y+X-W)/denominator);
+        backLeftMotor.setPower((Y-X+W)/denominator);
     }
 }
