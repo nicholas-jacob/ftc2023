96fa9e86e5ad20b233016e9237be42f8f5ac5f3c
nicholas-jacob <nicklefish7@gmail.com>
2023-01-29 16:22:51 -0700

    implemented the kookybots arm pid tutorial. both arm and tower controll is done and still untested. errors in the old parts of the gradle file think those were there from the start and have been ignored but watch out if we are having problems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2023V3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2023V3.java
@@ -1,25 +1,49 @@
 package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Config
+@TeleOp
 public class TeleOp2023V3 extends OpMode{
 
     //DT
-    private DcMotorEx frontRightMotor=null;
-    private DcMotorEx frontLeftMotor=null;
-    private DcMotorEx backRightMotor=null;
-    private DcMotorEx backLeftMotor=null;
+    //used to have =null but dont think that is nesecary now
+    private MecanumDrive mecanum;
+    private DcMotorEx frontRightMotor;
+    private DcMotorEx frontLeftMotor;
+    private DcMotorEx backRightMotor;
+    private DcMotorEx backLeftMotor;
 
     //TW
-    private DcMotorEx towerRight=null;
-    private DcMotorEx towerLeft=null;
+    private PIDController twController;
+    public static double Tp = 0, Ti = 0, Td = 0;
+    public static double Tf = 0;
+
+    public static int twTarget = 0;
+    private final double ticksPerMM = 1.503876;
+    private DcMotorEx towerRight;
+    private DcMotorEx towerLeft;
 
     //ARM
-    private DcMotorEx armMotor = null;
+    private PIDController armController;
+
+    public static double Ap = 0, Ai = 0, Ad = 0;
+    public static double Af = 0;
+
+    public static int armTarget = 0;
+    private final double ticksPerRadian = 28*(2.89655)*(3.61905)*(5.23077)*(2.4)/(2*Math.PI);
+    private DcMotorEx armMotor;
+
 
     //GR
     private Servo gripperRotationServo = null;
@@ -47,24 +71,25 @@ public class TeleOp2023V3 extends OpMode{
 
 
         //setUp tower
+        twController = new PIDController(Tp, Ti, Td);
         towerRight=hardwareMap.get(DcMotorEx.class, "towerRight");
         towerLeft=hardwareMap.get(DcMotorEx.class, "towerLeft");
-        PID tower=new PID(towerRight,towerLeft);
-
 
         towerRight.setDirection(DcMotorEx.Direction.FORWARD);
         towerLeft.setDirection(DcMotorEx.Direction.REVERSE);
-        tower.setValues(1,0,0);
-        tower.Iterate(100);
+        towerRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        towerLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        towerRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        towerLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+
 
         //setUp arm
+        armController = new PIDController(Ap, Ai, Ad);
         armMotor=hardwareMap.get(DcMotorEx.class, "armMotor");
-        PID arm=new PID(armMotor);
 
-        armMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        arm.setValues(1,0,0);
-        arm.Iterate(100);
-
+        armMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        armMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        armMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
 
 
@@ -76,8 +101,8 @@ public class TeleOp2023V3 extends OpMode{
 
 
 
-
-
+        //ftc dashboard stuff
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
     }
 
     public void init_loop() {
@@ -90,5 +115,41 @@ public class TeleOp2023V3 extends OpMode{
     @Override
     public void loop() {
 
+        //dt code
+        mecanum.Drive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+        //tower controller
+        twController.setPID(Tp, Ti, Td);
+        int towerPos = towerRight.getCurrentPosition();
+        double towerPid = twController.calculate(towerPos, twTarget);
+        double towerFf = Tf;
+
+        double towerPower = towerPid + towerFf;
+        towerRight.setPower(towerPower);
+        towerLeft.setPower(towerPower);
+
+        //arm controller
+        armController.setPID(Ap, Ai, Ad);
+        int armPos = armMotor.getCurrentPosition();
+        double armPid = armController.calculate(armPos, armTarget);
+        double armFf = Math.cos(armTarget / ticksPerRadian) * Af;
+
+        double armPower = armPid + armFf;
+        armMotor.setPower(armPower);
+
+
+
+
+
+
+
+
+        //uncomment for arm tuning
+        telemetry.addData("armPos", armPos);
+        telemetry.addData("armTarget", armTarget);
+
+
+
+        telemetry.update();
     }
 }
