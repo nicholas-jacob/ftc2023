9a5a8f87313b52be0ea7e6b14341ee52ec407c83
nicholas-jacob <nicklefish7@gmail.com>
2023-02-13 17:49:51 -0700

    Revert "stuff"
    This reverts commit 6b8ad2d8a1d2dd5907df13276a647694c7b3a8a1.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/cameraCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/cameraCode.java
@@ -19,26 +19,34 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.OpenCV;
 
-
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.OpenCV.AprilTagDetectionPipeline;
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 import java.util.ArrayList;
 
 @Autonomous
-public abstract class rightAuto extends OpMode {
+public class cameraCode extends OpMode {
 
     int state=0;
     //camera stuff
@@ -71,33 +79,15 @@ public abstract class rightAuto extends OpMode {
     SampleMecanumDrive drive = null;
 
     //points of interest
-    Pose2d startPose = new Pose2d(0, 0, Math.toRadians(0));
-    Pose2d leftParkPose = new Pose2d( -36, 60, Math.toRadians(0));
-    Pose2d middleParkPose = new Pose2d (0, 27, Math.toRadians(0));
-    Pose2d rightParkPose = new Pose2d (36, 12, Math.toRadians(0));
-    Trajectory middlePark = drive.trajectoryBuilder(startPose)
-            .lineToLinearHeading(middleParkPose)
-            .addDisplacementMarker(() -> {
-                state+=1;
-            })
 
-            .build();
-    Trajectory leftPark = drive.trajectoryBuilder(startPose)
-            .addDisplacementMarker(() -> drive.followTrajectoryAsync(middlePark))
-            .lineToLinearHeading(leftParkPose)
-            .addDisplacementMarker(() -> {
-                state+=1;
-            })
-            .build();
-    Trajectory rightPark = drive.trajectoryBuilder(startPose)
-            .addDisplacementMarker(() -> drive.followTrajectoryAsync(middlePark))
-            .lineToLinearHeading(rightParkPose)
-            .addDisplacementMarker(() -> {
-                state+=1;
-            })
-            .build();
+    Pose2d startPose=null;
+    Pose2d leftParkPose=null;
+    Pose2d middleParkPose=null;
+    Pose2d rightParkPose=null;
+    Trajectory middlePark=null;
+    Trajectory leftPark=null;
+    Trajectory rightPark=null;
 
-    //finite state machine
 
 
     public void init() {
@@ -124,9 +114,40 @@ public abstract class rightAuto extends OpMode {
         telemetry.setMsTransmissionInterval(50);
 
 
+        //roadrunner stuff
+
+        //roadrunner setup
+        drive = new SampleMecanumDrive(hardwareMap);
+
+
+        startPose = new Pose2d(0, 0, Math.toRadians(0));
+        leftParkPose = new Pose2d( 27, 24);
+        middleParkPose = new Pose2d (27, 0);
+        rightParkPose = new Pose2d (27, -24);
+        middlePark = drive.trajectoryBuilder(startPose)
+                .lineToLinearHeading(middleParkPose)
+                .addDisplacementMarker(() -> {
+                    state+=1;
+                })
+
+                .build();
+        leftPark = drive.trajectoryBuilder(middlePark.end())
+                .lineToLinearHeading(leftParkPose)
+                .addDisplacementMarker(() -> {
+                    state+=1;
+                })
+                .build();
+        rightPark = drive.trajectoryBuilder(middlePark.end())
+                .lineToLinearHeading(rightParkPose)
+                .addDisplacementMarker(() -> {
+                    state+=1;
+                })
+                .build();
+        drive.setPoseEstimate(startPose);
 
 
     }
+
 
     public void init_loop() {
 
@@ -159,18 +180,27 @@ public abstract class rightAuto extends OpMode {
                 }
             }
 
-        } else {
             telemetry.addLine("Don't see tag of interest :(");
 
-            if (tagOfInterest == null) {
-                telemetry.addLine("(The tag has never been seen)");
-            } else {
-                telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
-                //tagToTelemetry(tagOfInterest);
-            }
+
 
         }
-
+        if (tagOfInterest != null) {
+            if (tagOfInterest.id == left) {
+                telemetry.addLine("will go left");
+                signal = left;
+            } else if (tagOfInterest.id == right) {
+                telemetry.addLine("will go right");
+                signal = right;
+                //tagToTelemetry(tagOfInterest);
+            } else if (tagOfInterest.id == middle) {
+                telemetry.addLine("will go middle");
+                signal = middle;
+            }
+        } else {
+            telemetry.addLine("will go middle Bc didn't find anything");
+            signal = middle;
+        }
         telemetry.update();
     }
 
@@ -192,17 +222,7 @@ public abstract class rightAuto extends OpMode {
             telemetry.update();
         }
 
-        if (tagOfInterest.id == left) {
-            signal = left;
-        } else if (tagOfInterest == null || tagOfInterest.id == middle) {
-            signal = middle;
-        } else if (tagOfInterest.id == left) {
-            signal = right;
-        }
 
-        //roadrunner setup
-        drive = new SampleMecanumDrive(hardwareMap);
-        drive.setPoseEstimate(startPose);
 
         //finite state machine reset
     }
@@ -210,11 +230,12 @@ public abstract class rightAuto extends OpMode {
 
     public void loop(){
         if (state==0){
+            drive.followTrajectoryAsync(middlePark);
+            state+=1;
+        }
+        else if (state==2){
             if (signal == left) {
                 drive.followTrajectoryAsync(leftPark);
-            }
-            else if (signal == middle) {
-                drive.followTrajectoryAsync(middlePark);
             }
             else if (signal == right) {
                 drive.followTrajectoryAsync(rightPark);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -29,17 +29,16 @@ import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.lynx.LynxModule;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceRunner;
@@ -54,10 +53,10 @@ import java.util.List;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(1.5, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(2.75, 0, 0  );
 
-    public static double LATERAL_MULTIPLIER = 1;
+    public static double LATERAL_MULTIPLIER = 0.8929;
 
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
@@ -73,7 +72,7 @@ public class SampleMecanumDrive extends MecanumDrive {
     private DcMotorEx leftFront, leftRear, rightRear, rightFront;
     private List<DcMotorEx> motors;
 
-    private IMU imu;
+    private BNO055IMU imu;
     private VoltageSensor batteryVoltageSensor;
 
     public SampleMecanumDrive(HardwareMap hardwareMap) {
@@ -91,18 +90,37 @@ public class SampleMecanumDrive extends MecanumDrive {
         }
 
         // TODO: adjust the names of the following hardware devices to match your configuration
-        imu = hardwareMap.get(IMU.class, "imu");
-        // TODO: Adjust the orientations here to match your robot. See the FTC SDK documentation for
-        // details
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
         imu.initialize(parameters);
 
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
-        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        // TODO: If the hub containing the IMU you are using is mounted so that the "REV" logo does
+        // not face up, remap the IMU axes so that the z-axis points upward (normal to the floor.)
+        //
+        //             | +Z axis
+        //             |
+        //             |
+        //             |
+        //      _______|_____________     +Y axis
+        //     /       |_____________/|__________
+        //    /   REV / EXPANSION   //
+        //   /       / HUB         //
+        //  /_______/_____________//
+        // |_______/_____________|/
+        //        /
+        //       / +X axis
+        //
+        // This diagram is derived from the axes in section 3.4 https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bno055-ds000.pdf
+        // and the placement of the dot/orientation from https://docs.revrobotics.com/rev-control-system/control-system-overview/dimensions#imu-location
+        //
+        // For example, if +Y in this diagram faces downwards, you would use AxisDirection.NEG_Y.
+        // BNO055IMUUtil.remapZAxis(imu, AxisDirection.NEG_Y);
+
+        leftFront = hardwareMap.get(DcMotorEx.class, "motorFrontLeft");
+        leftRear = hardwareMap.get(DcMotorEx.class, "motorBackLeft");
+        rightRear = hardwareMap.get(DcMotorEx.class, "motorBackRight");
+        rightFront = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
 
         motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
 
@@ -123,6 +141,8 @@ public class SampleMecanumDrive extends MecanumDrive {
         }
 
         // TODO: reverse any motors using DcMotor.setDirection()
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // TODO: if desired, use setLocalizer() to change the localization method
         // for instance, setLocalizer(new ThreeTrackingWheelLocalizer(...));
@@ -276,12 +296,12 @@ public class SampleMecanumDrive extends MecanumDrive {
 
     @Override
     public double getRawExternalHeading() {
-        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        return imu.getAngularOrientation().firstAngle;
     }
 
     @Override
     public Double getExternalHeadingVelocity() {
-        return (double) imu.getRobotAngularVelocity(AngleUnit.RADIANS).zRotationRate;
+        return (double) imu.getAngularVelocity().zRotationRate;
     }
 
     public static TrajectoryVelocityConstraint getVelocityConstraint(double maxVel, double maxAngularVel, double trackWidth) {
