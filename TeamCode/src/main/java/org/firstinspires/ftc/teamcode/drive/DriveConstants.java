8ef40d2287e2dec9e775998bc5307811ac5f0bc6
nicholas-jacob <nicklefish7@gmail.com>
2023-02-15 21:21:32 -0700

    fully finished roadrunner tuning (using feedforeward control) (issue with the 10 degree undershoot ended up being enabled heading pid while trying to tune which is solved now)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -31,8 +31,8 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(32, 0, 8, 13.5);
+    public static final boolean RUN_USING_ENCODER = false;
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0, 0);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -45,7 +45,7 @@ public class DriveConstants {
     public static double WHEEL_RADIUS = 1.8898; // in
     //mutilpled by gear ratio/final x from straight test because distance traveled was off
     public static double GEAR_RATIO = 1.182627; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 11.2; // in
+    public static double TRACK_WIDTH = 15.8; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -53,9 +53,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.014;
+    public static double kA = 0.003;
+    public static double kStatic = 0.05;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
