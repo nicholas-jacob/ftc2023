96641a654290d5b17092d2dedfba9fca97d32321
FTC-TosterMedics <danech.veeranshu@gmail.org>
2023-02-22 15:26:47 -0700

    Meep Meep test working

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,8 +13,8 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(24, 20, Math.toRadians(160), Math.toRadians(140), 15.8)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-36, (-72+6.23), Math.toRadians(180)))
-                                .splineToSplineHeading(new Pose2d(-36, -24, Math.toRadians(180)), Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-38.3125, (-72+6.23), Math.toRadians(180)))
+                                .lineToSplineHeading(new Pose2d(-36, -24, Math.toRadians(180)))
                                 .splineToSplineHeading(new Pose2d(-56, -8, Math.toRadians(194.0362)), Math.toRadians(14.0362))
 
                                 .build()
